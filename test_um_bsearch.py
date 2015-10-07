84bc5c7e1a46b721d442433c8750098e0b0add20
mskmoorthy <mskmoorthy@gmail.com>
2015-10-07 17:03:53 -0400

    added one more unit test

--- test_um_bsearch.py
+++ test_um_bsearch.py
@@ -1,11 +1,11 @@
 import unittest
+import random
 from bsearch import bsearch
  
 class TestUM(unittest.TestCase):
  
     def setUp(self):
-        pass
- 
+        pass 
     def test_0(self):
         a=[]
         a.sort()
@@ -15,6 +15,11 @@ class TestUM(unittest.TestCase):
         a=[12,23,56,78,9,-90,4]
         a.sort()
         self.assertEqual( bsearch(a,9), 1)
+    def test_big_array(self):
+        print("In big array test")
+        a = [random.randint(1,10000) for _ in xrange(1000000)]
+        a.sort()
+        self.assertEqual(bsearch(a,20000),0)
  
 if __name__ == '__main__':
     unittest.main()
